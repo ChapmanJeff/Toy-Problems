028393b83d000ae4e4b1730b2a838cb4396ea0c7
Jeff Chapman <jeffcchapman@gmail.com>
2015-01-20 09:03:08 -0700

    finished toy6

---
+++ toy6.js
@@ -0,0 +1,12 @@
+//Write a function that returns the factorial of a number
+//the number 5 will return 5*4*3*2*1
+
+var func = function (num) {
+	var newNum = 1;
+	for (var i = num; i >= 1; i--) {
+		newNum = newNum * i;
+	}
+	return newNum;
+}
+
+func(3);
