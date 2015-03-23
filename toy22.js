bf4163a9501b8621df3811dca64a8f2f57f2b634
Jeff Chapman <jeffcchapman@gmail.com>
2015-03-23 09:47:11 -0600

    22

---
+++ toy22.js
@@ -0,0 +1,9 @@
+var isPalindrome (str) {
+	var string = str.split(' ').join('')reverse().join('').toLowerCase();
+	str = str.split(' ').join('').toLowerCase();
+	if(string === str) {
+		console.log(true)
+	}
+	console.log(false)
+}
+
